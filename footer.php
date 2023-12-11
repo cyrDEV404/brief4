354c1450c7886f4129e1a924d54bd6b7ef355b59
sacleuxa <147699402+sacleuxa@users.noreply.github.com>
2023-12-11 14:56:27 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,10 +2,18 @@
     <footer> 
         <container> 
             <row> 
-             <div class="column"></div>
-             <div class="column"></div>
-             <div class="column"></div>
-             <div class="colum"></div>
+             <div class="column">
+                <div class="h3"> À propos</div>
+             </div>
+             <div class="column">
+                <div class="h3"> Profils </div>
+             </div>
+             <div class="column">
+                <div class="h3"> Informations</div>
+             </div>
+             <div class="column">
+                <div class="h3">Notre site</div>
+             </div>
             </row>
         </container>
     </footer>
