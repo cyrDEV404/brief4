d042a9ed4e09828736ed7ce1cac4668443ab8fa5
sacleuxa <147699402+sacleuxa@users.noreply.github.com>
2023-12-11 14:41:41 +0100

    columns for footer

---
+++ footer.php
@@ -0,0 +1,12 @@
+<html> 
+    <footer> 
+        <container> 
+            <row> 
+             <div class="column"></div>
+             <div class="column"></div>
+             <div class="column"></div>
+             <div class="colum"></div>
+            </row>
+        </container>
+    </footer>
+</html>
