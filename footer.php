727c38951cbac2e8aaffe42aae15807e55cc9dca
sacleuxa <147699402+sacleuxa@users.noreply.github.com>
2023-12-12 16:55:40 +0100

    footer php index css style update

--- footer.php
+++ footer.php
@@ -1,97 +1,54 @@
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/script.js" defer></script>
-    <title>brief4-trombinoscope</title>
-</head>
-    <footer> 
-        <container> 
-            <row> 
-             <div class="column">
-             <a href= "https://www.meformerenregion.fr/organismes/greta-cfa-de-lozere-mende" class="H3"> GRETA </a>
-             </div>
-             <div class="column">
-                <div class="h3"> Profils </div>
-             </div>
-             <div class="column">
-                <div class="h3"> Informations</div>
-             </div>
-             <div class="column">
-                <div class="h3">Notre site</div>
-                
-             </div>
-            </row>
-        </container>
-    </footer>
-</body>
-</html>
 <footer class="container">
         <span style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
             <row> 
                 <div class="column">
-                <a href= "https://www.meformerenregion.fr/organismes/greta-cfa-de-lozere-mende" class="H3"> GRETA </a>
+                <h3> GRETA </h3>
                 <ul class="footer__address">
                    <li>
-                    Lycée Émile Peytavin, 48000 Mende
-                   </li>
-                   <li>
-                       +33 04 66 65 25 24
-                   </li>
-                   <li>
-                      <a href="https://www.meformerenregion.fr/contact"> Formulaire de contact </a>
+                    <p> Lycée Émile Peytavin, 48000 Mende <br>
+                        +33 04 66 65 25 24 <br>
+                        <a href="https://www.meformerenregion.fr/contact"> À Propos </a> 
+                   </p>
                    </li>
                </ul>
                 </div>
                 <div class="column">
-                    <a href= "https://www.cariforefoccitanie.fr/" class="H3"> CARIF OREF </a>
+                    <h3> CARIF OREF </h3>
                    <ul class="footer__address">
                        <li>
-                        19 Rue Carmin, 31670 Toulouse
-                       </li>
-                       <li>
-                           +33 05 62 24 05 99
-                       </li>
-                       <li>
-                        http://www.cariforefoccitanie.fr
+                        <p> 19 Rue Carmin, 31670 Toulouse <br>
+                            +33 05 62 24 05 99 <br>
+                            <a href= "http://www.cariforefoccitanie.fr"> À Propos </a>
+                        </p>
                        </li>
                    </ul>
                    </div>
                 </div>
                 <div class="column">
-                   <a href= "https://polen-mende.com/" class="H3"> POLEN </a>
+                   <h3> POLEN </h3>
                    <ul class="footer__address">
                        <li>
-                         Rue Albert Einstein, Mende 48000
-                       </li>
-                       <li>
-                           +33 04 66 45 33 02
-                       </li>
-                       <li>
-                        v.gatin@lozere-developpement.com
+                        <p>
+                            Rue Albert Einstein, 48000 Mende <br>
+                            +33 04 66 45 33 02 <br>
+                            <a href= "https://polen-mende.com/"> À Propos </a>
+                        </p>
                        </li>
                    </ul>
                 </div>
                 <div class="column">
-                   <h3> PORTFOLIOS </H3>
-                   <ul class="footer__address">
+                   <H3> PORTFOLIOS </H3>
+                   <ul class="footer__address__portfolio">
                        <li>
-                           43 Raymouth Rd. Baltemoer, London 3910
-                       </li>
-                       <li>
-                           +1(123-456-7890)
-                       </li>
-                       <li>
-                           +1(123)-456-7890
-                       </li>
-                       <li>
-                           info@mydomain.com
+                        <p>
+                            Dans cette collection de portfolios, vous trouverez un aperçu des diverses compétences de nos talentueux <a href= "portfolios.ern-mende.fr"> diplômés.</a>
+                        </p>
                        </li>
                    </ul>
                 </div>
                </row>
         </span>
 </footer>
+</body>
+</html>
