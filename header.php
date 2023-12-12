d2aaa771e5be1351d875f650c4c98fcc9d3a73b4
cyrDEV404 <147699426+cyrDEV404@users.noreply.github.com>
2023-12-12 15:18:20 +0100

    update

--- header.php
+++ header.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/script.js" defer></script>
-    <title>brief4-trombinoscope</title>
-</head>
-
 <body>
 <div class="card-holder">
   <img src="https://picsum.photos/id/237/280/250" alt="Image 1">
