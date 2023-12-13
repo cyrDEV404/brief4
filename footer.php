067c8c5bbd774c9d8520a38bd2eaf25bd4d328e1
martinched <147699516+martinched@users.noreply.github.com>
2023-12-13 16:37:00 +0100

    footer resolu

--- footer.php
+++ footer.php
@@ -1,53 +1,53 @@
 <footer class="container">
-      <span style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
-          <row> 
-              <div class="column">
-              <h3> GRETA </h3>
-              <ul class="footer__address">
-                 <li>
-                  <p> Lycée Émile Peytavin, 48000 Mende <br>
-                      +33 04 66 65 25 24 <br>
-                      <a href="https://www.meformerenregion.fr/contact"> À Propos </a> 
-                 </p>
-                 </li>
-             </ul>
-              </div>
-              <div class="column">
-                  <h3> CARIF OREF </h3>
-                 <ul class="footer__address">
-                     <li>
-                      <p> 19 Rue Carmin, 31670 Toulouse <br>
-                          +33 05 62 24 05 99 <br>
-                          <a href= "http://www.cariforefoccitanie.fr"> À Propos </a>
-                      </p>
-                     </li>
-                 </ul>
-                 </div>
-              </div>
-              <div class="column">
-                 <h3> POLEN </h3>
-                 <ul class="footer__address">
-                     <li>
-                      <p>
-                          Rue Albert Einstein, 48000 Mende <br>
-                          +33 04 66 45 33 02 <br>
-                          <a href= "https://polen-mende.com/"> À Propos </a>
-                      </p>
-                     </li>
-                 </ul>
-              </div>
-              <div class="column">
-                 <H3> PORTFOLIOS </H3>
-                 <ul class="footer__address__portfolio">
-                     <li>
-                      <p>
-                          Vous trouverez un aperçu des diverses compétences de nos talentueux <a href= "portfolios.ern-mende.fr"> diplômés.</a>
-                      </p>
-                     </li>
-                 </ul>
-              </div>
-             </row>
-      </span>
-</footer>
+        <div class='style__footer'>
+                <div class="column">
+                    <h3> GRETA </h3>
+                    <ul class="footer__address">
+                        <li>
+                            <p> Lycée Émile Peytavin, 48000 Mende <br>
+                                 +33 04 66 65 25 24 <br>
+                                <a href="https://www.meformerenregion.fr/contact"> À Propos </a> 
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="column">
+                    <h3> CARIF OREF </h3>
+                    <ul class="footer__address">
+                        <li>
+                            <p> 19 Rue Carmin, 31670 Toulouse <br>
+                            +33 05 62 24 05 99 <br>
+                            <a href= "http://www.cariforefoccitanie.fr"> À Propos </a>
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+             
+                <div class="column">
+                    <h3> POLEN </h3>
+                    <ul class="footer__address">
+                        <li>
+                            <p>
+                                Rue Albert Einstein, 48000 Mende <br>
+                                +33 04 66 45 33 02 <br>
+                                <a href= "https://polen-mende.com/"> À Propos </a>
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="column">
+                    <H3> PORTFOLIOS </H3>
+                        <ul class="footer__address__portfolio">
+                            <li>
+                                <p>
+                                    Vous trouverez un aperçu des diverses compétences de nos talentueux<br> <a href= "portfolios.ern-mende.fr"> diplômés.</a>
+                                </p>
+                            </li>
+                        </ul>
+                </div>
+        </div>
+    </footer>
 </body>
 </html>
