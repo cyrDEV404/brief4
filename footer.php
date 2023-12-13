1009406b6eda31fb54a8992f7b114e459d8daafc
cyrDEV404 <147699426+cyrDEV404@users.noreply.github.com>
2023-12-13 09:39:04 +0100

    Update

--- footer.php
+++ footer.php
@@ -1,34 +1,3 @@
-
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/script.js" defer></script>
-    <title>brief4-trombinoscope</title>
-</head>
-    <footer> 
-        <container> 
-            <row> 
-             <div class="column">
-             <a href= "https://www.meformerenregion.fr/organismes/greta-cfa-de-lozere-mende" class="H3"> GRETA </a>
-             </div>
-             <div class="column">
-                <div class="h3"> Profils </div>
-             </div>
-             <div class="column">
-                <div class="h3"> Informations</div>
-             </div>
-             <div class="column">
-                <div class="h3">Notre site</div>
-                
-             </div>
-            </row>
-        </container>
-    </footer>
-</body>
-</html>
 <footer class="container">
         <span style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
             <row> 
