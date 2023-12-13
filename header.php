aa7030bbf8d68e4e4aa559807d47e28102432c8d
martinched <147699516+martinched@users.noreply.github.com>
2023-12-13 10:25:12 +0100

    with a docttype in the header

--- header.php
+++ header.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="script.js" defer></script>
+    <title>brief4-trombinoscope</title>
+</head>
 <body>
 <header> 
     <div>
