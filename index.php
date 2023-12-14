d1b2ef71c1f012027f49a2fc47ee1276551a2d7f
cyrDEV404 <147699426+cyrDEV404@users.noreply.github.com>
2023-12-14 12:18:57 +0100

    Update

--- index.php
+++ index.php
@@ -1,71 +1,124 @@
 <?php include('header.php') ?>
-
-    <section>
+<section>
+        <div>
         <div class="card-holder">
+            <p></p>
             <img src="https://picsum.photos/id/237/280/250" alt="Image 1">
-            <h3>Card 1</h3>
-            <a href="https://www.linkedin.com/"><img class="icon_img" src="image/linkedin.png" alt=""></a>
+            <h3>Card 1 NAME</h3>            
+            <a class="" href="https://www.linkedin.com/">
+                <img class="" src="image/linkedin.png" alt="">
+                <img class="icon_img" src="image/github.png" alt="">
+            </a>
         </div>
           
           <div class="card-holder">
             <img src="https://picsum.photos/id/238/280/250" alt="Image 2">
             <h3>Card 2</h3>
+            <a class="" href="https://www.linkedin.com/">
+                <img class="" src="image/linkedin.png" alt="">
+                <img class="icon_img" src="image/github.png" alt="">
+            </a>
           </div>
           
           <div class="card-holder">
             <img src="https://picsum.photos/id/239/280/250" alt="Image 3">
             <h3>Card 3</h3>
+            <a class="" href="https://www.linkedin.com/">
+                <img class="" src="image/linkedin.png" alt="">
+                <img class="icon_img" src="image/github.png" alt="">
+            </a>
           </div>
           
           <div class="card-holder">
             <img src="https://picsum.photos/id/240/280/250" alt="Image 4">
             <h3>Card 4</h3>
+            <a class="" href="https://www.linkedin.com/">
+                <img class="" src="image/linkedin.png" alt="">
+                <img class="icon_img" src="image/github.png" alt="">
+            </a>
           </div>
 
           <div class="card-holder">
             <img src="https://picsum.photos/id/237/280/250" alt="Image 1">
-            <h3>Card 1</h3>
+            <h3>Card 5</h3>
+            <a class="" href="https://www.linkedin.com/">
+                <img class="" src="image/linkedin.png" alt="">
+                <img class="icon_img" src="image/github.png" alt="">
+            </a>
           </div>
           
           <div class="card-holder">
             <img src="https://picsum.photos/id/238/280/250" alt="Image 2">
-            <h3>Card 2</h3>
+            <h3>Card 6</h3>
+            <a class="" href="https://www.linkedin.com/">
+                <img class="" src="image/linkedin.png" alt="">
+                <img class="icon_img" src="image/github.png" alt="">
+            </a>
           </div>
           
           <div class="card-holder">
             <img src="https://picsum.photos/id/239/280/250" alt="Image 3">
-            <h3>Card 3</h3>
+            <h3>Card 7</h3>
+            <a class="" href="https://www.linkedin.com/">
+                <img class="" src="image/linkedin.png" alt="">
+                <img class="icon_img" src="image/github.png" alt="">
+            </a>
           </div>
           
           <div class="card-holder">
             <img src="https://picsum.photos/id/240/280/250" alt="Image 4">
-            <h3>Card 4</h3>
+            <h3>Card 8</h3>
+            <a class="" href="https://www.linkedin.com/">
+                <img class="" src="image/linkedin.png" alt="">
+                <img class="icon_img" src="image/github.png" alt="">
+                
+            </a>
           </div>
 
           <div class="card-holder">
             <img src="https://picsum.photos/id/237/280/250" alt="Image 1">
-            <h3>Card 1</h3>
+            <h3>Card 9</h3>
+            <a class="" href="https://www.linkedin.com/">
+                <img class="" src="image/linkedin.png" alt="">
+                <img class="icon_img" src="image/github.png" alt="">
+            </a>
           </div>
           
           <div class="card-holder">
             <img src="https://picsum.photos/id/238/280/250" alt="Image 2">
-            <h3>Card 2</h3>
+            <h3>Card 10</h3>
+            <a class="" href="https://www.linkedin.com/">
+                <img class="" src="image/linkedin.png" alt="">
+                <img class="icon_img" src="image/github.png" alt="">
+            </a>
           </div>
           
           <div class="card-holder">
             <img src="https://picsum.photos/id/239/280/250" alt="Image 3">
-            <h3>Card 3</h3>
+            <h3>Card 11</h3>
+            <a class="" href="https://www.linkedin.com/">
+                <img class="" src="image/linkedin.png" alt="">
+                <img class="icon_img" src="image/github.png" alt="">
+            </a>
           </div>
           
           <div class="card-holder">
             <img src="https://picsum.photos/id/240/280/250" alt="Image 4">
-            <h3>Card 4</h3>
+            <h3>Card 12</h3>
+            <a class="" href="https://www.linkedin.com/">
+                <img class="" src="image/linkedin.png" alt="">
+                <img class="icon_img" src="image/github.png" alt="">
+            </a>
           </div>
 
           <div class="card-holder">
             <img src="https://picsum.photos/id/240/280/250" alt="Image 4">
-            <h3>Card 4</h3>
+            <h3>Card 13</h3>
+            <a class="" href="https://www.linkedin.com/">
+                <img class="" src="image/linkedin.png" alt="">
+                <img class="icon_img" src="image/github.png" alt="">
+            </a>
           </div>
+        </div>
     </section>
-
 <?php include('footer.php') ?>
--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-const apiUrl = 'https://portfolios.ern-mende.fr/wp-json/wp/v2/apprenants';
-const dataContainer = document.getElementById('dataContainer');
+const apiUrl = 'https://portfolios.ern-mende.fr/wp-json/wp/v2/apprenants/';
+const dataImage = document.getElementById('dataImage');
+const dataContenuCard = document.getElementById('dataContenuCard');
 
 fetch(apiUrl)
+
 .then(response => {
   if (!response.ok) {
     throw new Error('Network response was not ok');
-    console.log(response)
   }
   return response.json();
 })
 
-.then(data => {
-  // Traitement des données récupérées
-  console.log(data)
-  dataContainer.src = data.image;
-})
+.then(data => {  
+  if(data[0].image == "") {
+    dataImage.src = "https://cdn.shopify.com/s/files/1/0490/8384/2713/files/2.-Le-chat-Bengal-le-leopard_600x600.jpg?v=1672839840"
+  }else{
+    console.log(data)
+    dataImage.src = data[0].image;
 
+  }data.forEach(afficheData); 
+    dataImage.
+})
 .catch(error => {
   console.error('There was a problem with the fetch operation:', error);
 });
 
+function afficheData(data){
+  var  dataPromo = document.getElementById('dataPromo');
+  dataImage.src = data.image
+
+  var elementDiv = document.createElement("div");
+  var p = document.createElement("p");
+  document.body.appendChild(p);
+
+  
+  
+
+  
+
+
+};
